7a3fbaeff5b3b437b75bf0ab40e1e95aed18eab9
TJ-2004 <124814940+TJ-2004@users.noreply.github.com>
2024-02-02 19:03:50 +0530

    Fix

--- js/script.js
+++ js/script.js
@@ -85,7 +85,7 @@ const playMusic = (track, pause = false) => {
 };
 
 async function displayAlbum() {
-  let a = await fetch(`songs/`);
+  let a = await fetch(`/songs/`);
   let response = await a.text();
   let div = document.createElement("div");
   div.innerHTML = response;
@@ -100,7 +100,7 @@ async function displayAlbum() {
     if (e.href.includes("/songs") && !e.href.includes(".htaccess")) {
       let folder = e.href.split("/").slice(-2)[0];
       //Get the metadata of each folder
-      let a = await fetch(`songs/${folder}/info.json`);
+      let a = await fetch(`/songs/${folder}/info.json`);
       let response = await a.json();
       //console.log(response);
 
